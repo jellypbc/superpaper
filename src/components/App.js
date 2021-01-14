04c353c67f4866dd6c37457c64cd29d14ffb587a
cindy <ciindy.wu@gmail.com>
2021-01-13 16:18:49 -1000

    Adds description, request early access button, and twitter to landing page

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div id="app">
       <h1>superpaper</h1>
+      <p>a new kind of paper awaits</p>
+      <a href="https://cindy308194.typeform.com/to/hxiMdJ79" className="btn">request early access</a>
+      <p><a href="http://twitter.com/superpaperapp">@superpaperapp</a></p>
     </div>
   )
 }
